ea346a3b2faeb04296342596d1757fd1149c6bf3
Hitesh Suthar <hiteshsuthar1410@gmail.com>
2026-06-17 23:21:01 +0530

    Initial Commit

---
+++ Eulerity/EulerityApp.swift
@@ -0,0 +1,17 @@
+//
+//  EulerityApp.swift
+//  Eulerity
+//
+//  Created by Hitesh Suthar on 17/06/26.
+//
+
+import SwiftUI
+
+@main
+struct EulerityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
